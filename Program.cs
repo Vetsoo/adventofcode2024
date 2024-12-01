e50a25e8f08e21f1f292d66c06b37b0d19deef40
Jente Vets <jente.vets@reynaers.com>
2024-12-01 08:59:40 +0100

    Init setup

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace adventofcode2023;
+
+static class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Advent of code 2024!");
+        await Day1();
+    }
+
+    static async Task Day1()
+    {
+        Console.WriteLine("Welcome to the DAY 1!");
+        Console.WriteLine("");
+        Console.WriteLine("Started reading input...");
+        var inputText = File.ReadLinesAsync(@"input/day1.txt");
+        //var calibrationValues = inputText.Select(l => int.Parse($"{l.First(c => int.TryParse(c.ToString(), out int f))}{l.Last(c => int.TryParse(c.ToString(), out int f))}"));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # adventofcode2024
-Advent Of Code 2024
+ 
