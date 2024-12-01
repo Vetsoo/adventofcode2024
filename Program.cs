827b527fa2237860d5b755a210cdc9016ddaddae
Jente Vets <jente.vets@reynaers.com>
2024-12-01 09:44:30 +0100

    Day 1 part 1

--- Program.cs
+++ Program.cs
@@ -8,12 +8,30 @@ static class Program
         await Day1();
     }
 
-    static async Task Day1()
+    static Task Day1()
     {
         Console.WriteLine("Welcome to the DAY 1!");
         Console.WriteLine("");
         Console.WriteLine("Started reading input...");
-        var inputText = File.ReadLinesAsync(@"input/day1.txt");
-        //var calibrationValues = inputText.Select(l => int.Parse($"{l.First(c => int.TryParse(c.ToString(), out int f))}{l.Last(c => int.TryParse(c.ToString(), out int f))}"));
+        var inputText = File.ReadAllLines(@"input/day1.txt");
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach (var line in inputText)
+        {
+            var splittedLine = line.Split("   ");
+            _ = int.TryParse(splittedLine[0].ToString(), out int l);
+            _ = int.TryParse(splittedLine[1].ToString(), out int r);
+            left.Add(l);
+            right.Add(r);
+        }
+        var orderLeft = left.OrderBy(x => x).ToList();
+        var orderedRight = right.OrderBy(x => x).ToList();
+
+        var sum = orderLeft.Select((x, i) => Math.Abs(x - orderedRight[i])).Sum();
+
+        Console.WriteLine($"Sum of differences: {sum}");
+
+        return Task.CompletedTask;
     }
 }
